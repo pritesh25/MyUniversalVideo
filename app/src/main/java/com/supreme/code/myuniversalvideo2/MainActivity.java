6dbeab5f737f61fa8a86daa24970a2ed04b1797a
Pritesh <Priteshvishwakarma@gmail.com>
2018-09-10 18:05:19 +0530

    comment added

--- app/src/main/java/com/supreme/code/myuniversalvideo2/MainActivity.java
+++ app/src/main/java/com/supreme/code/myuniversalvideo2/MainActivity.java
@@ -96,6 +96,7 @@ public class MainActivity extends AppCompatActivity implements UniversalVideoVie
             }
         });
     }
+
     /*
     @Override
     public void onScaleChange(boolean isFullscreen) {
